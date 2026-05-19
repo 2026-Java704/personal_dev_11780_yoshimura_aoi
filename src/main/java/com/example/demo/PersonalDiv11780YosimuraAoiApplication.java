5a7db0af6bfe8c37abce3e536364c8f9f47d6b1d
student <student@FJ-1025>
2026-05-19 11:27:13 +0900

    ファーストコミット

---
+++ src/main/java/com/example/demo/PersonalDiv11780YosimuraAoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalDiv11780YosimuraAoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalDiv11780YosimuraAoiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/PersonalDiv11780YosimuraAoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalDiv11780YosimuraAoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
